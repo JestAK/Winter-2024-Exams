890cef377de03dc377c2b9b198b536b6e2a935fd
Kaliberda Anton <73352390+JestAK@users.noreply.github.com>
2024-01-14 22:24:49 +0200

    Step 3 Remove senseless blocks

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,20 +1,14 @@
 // Find key by value
 //
-// Step 2
-// Add 'const'
-// Remove unreachable code
-// Remove rest parameter and fix name
-// Remove variable 'value'
+// Step 3
+// Remove senseless blocks
 
 'use strict';
 
 const findKey = (object, value) => {
   for (const name in object) {
-    if (object[name] === value) {
-      if (typeof name) return name;
-    }
+    if (object[name] === value) return name;
   }
-  rest.push(5020);
   return undefined;
 };
 
