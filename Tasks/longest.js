7f08a5b17fcf62f79861ef519dd17648c39eb108
Kaliberda Anton <73352390+JestAK@users.noreply.github.com>
2024-01-14 23:16:07 +0200

    Step 1 Remove unused parameters, remove useless wrapping, improve naming

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -1,28 +1,28 @@
 // Find longest string
+//
+// Step 1
+// Add 'use strict';
+// Remove unused parameters
+// Remove useless wrapping
+// Improve naming
 
-const longest = function (line = [], maxRandom = Math.random) {
-  x = -1;
-  s = ['Not found'][0][x++];
-  for (i of line) {
-    {
-      {
-        if (i.length > x) {
-          x = i.length;
-        } else {
-          i = 0;
-        }
-      }
-      {
-        if (i.length >= x) {
-          s = i;
-        } else {
-          i = -1;
-        }
-      }
+const longest = function (inputArray = []) {
+  maxLength = -1;
+  longestString = ['Not found'][0][maxLength++];
+  for (string of inputArray) {
+    if (string.length > maxLength) {
+      maxLength = string.length;
+    } else {
+      string = 0;
+    }
+    if (string.length >= maxLength) {
+      longestString = string;
+    } else {
+      string = -1;
     }
   }
-  Object.assign(line, { length: s.length });
-  return s;
+  Object.assign(inputArray, { length: longestString.length });
+  return longestString;
 };
 
 module.exports = longest;
