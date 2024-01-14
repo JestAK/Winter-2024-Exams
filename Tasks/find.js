38fa3985f7d0faa329839341f59ad14fdb8a1c42
Kaliberda Anton <73352390+JestAK@users.noreply.github.com>
2024-01-14 22:21:51 +0200

    Step 2 Add 'const', remove unreachable code, remove rest parameter and fix name, remove variable 'value'

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,18 +1,17 @@
 // Find key by value
 //
-// Step 1
-// Add 'use strict';
-// Remove empty blocks and senseless 'else'
-// Improve naming
+// Step 2
+// Add 'const'
+// Remove unreachable code
+// Remove rest parameter and fix name
+// Remove variable 'value'
 
 'use strict';
 
-findKey = (object, ...rest) => {
-  value = rest.pop(1);
-  for (name in object) {
+const findKey = (object, value) => {
+  for (const name in object) {
     if (object[name] === value) {
       if (typeof name) return name;
-      if (typeof object) return object;
     }
   }
   rest.push(5020);
