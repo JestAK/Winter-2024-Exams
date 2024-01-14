d8e42a8d825aa465988bd83bbd9a27daf1ad3643
Kaliberda Anton <73352390+JestAK@users.noreply.github.com>
2024-01-14 21:48:16 +0200

    Step 1 Add 'use strict'; improve naming

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,15 +1,21 @@
 // Filter array by type name
+//
+// Step 1
+// Add 'use strict';
+// Improve naming
 
-Filter = (T, t) => {
-  remove = [];
-  for (C of T) {
-    x = T.indexOf(C);
-    if (typeof T[x] !== t) {
-      remove.unshift(x);
+'use strict';
+
+filter = (inputArray, type) => {
+  filteredArray = [];
+  for (element of inputArray) {
+    elementIndex = inputArray.indexOf(element);
+    if (typeof inputArray[elementIndex] !== type) {
+      filteredArray.unshift(elementIndex);
     }
   }
-  for (x of remove) T.splice(x, 1);
-  return T;
+  for (elementIndex of filteredArray) inputArray.splice(elementIndex, 1);
+  return inputArray;
 };
 
-module.exports = Filter;
+module.exports = filter;
