4509271608c5430b92a7120aff484f5c7568628a
Kaliberda Anton <73352390+JestAK@users.noreply.github.com>
2024-01-14 23:37:36 +0200

    Step 3 Remove duplicated blocks, remake function to arrow function

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -1,21 +1,17 @@
 // Find longest string
 //
-// Step 2
-// Add 'use strict';
-// Add 'const' and 'let'
-// Remove useless blocks
-// Remove default value of parameter 'inputArray'
+// Step 3
+// Remove duplicated blocks
+// Remake function to arrow function
 
 'use strict';
 
-const longest = function (inputArray) {
+const longest = (inputArray) => {
   let maxLength = 0;
   let longestString;
   for (const string of inputArray) {
     if (string.length > maxLength) {
       maxLength = string.length;
-    }
-    if (string.length >= maxLength) {
       longestString = string;
     }
   }
