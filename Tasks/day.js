812f100e6f97f5a730d4dd533a446dd7df8165d9
Kaliberda Anton <73352390+JestAK@users.noreply.github.com>
2024-01-14 21:00:07 +0200

    Step 2 Add 'const', declare and initialize 'i' variable at one time, prefer const, minimize let usage

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,17 +1,16 @@
 // Get day number
 //
-// Step 1
-// Add 'use strict';
-// Fix formatting
-// Improve naming
+// Step 2
+// Add 'const'
+// Declare and initialize 'i' variable at one time
+// Prefer const, minimize let usage
 
 'use strict';
 
-let daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-parseDay = (string) => {
-  let i;
-  for (i = 0; i < daysOfWeek.length; i++) {
+const parseDay = (string) => {
+  for (let i = 0; i < daysOfWeek.length; i++) {
     if (string.startsWith(daysOfWeek[i].toLowerCase())) return i + 1;
   }
   return -1;
